858de297cd4958dceef224c090777382009c7b76
Darryl G. Baker <89161895+dfirdeferred@users.noreply.github.com>
2026-05-06 13:46:15 -0400

    Delete Other Versions/Go directory

--- Other Versions/Go/utils/credman_other.go
+++
@@ -1,8 +0,0 @@
-//go:build !windows
-
-package utils
-
-// QueryCredentialManager is not available on non-Windows platforms.
-func QueryCredentialManager(target string) (string, error) {
-	return "", ErrNotWindows
-}
